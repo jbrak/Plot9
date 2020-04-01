cc67947948ca2ec2d86dc82a3294f5ee0b38fa4f
Jbrak <joe@brakoniecki.com>
2020-04-01 12:42:21 +0100

    Updated

--- Sample.py
+++ Sample.py
@@ -6,4 +6,7 @@ from plotnine.data import *
 %matplotlib inline
 mpg.head()
 
+
 ggplot(mpg) + geom_bar(aes(x='class', fill = 'drv')) + theme_bw()
+
+ggplot(mpg, aes(x='displ', y='hwy')) + geom_point() + geom_smooth(method = 'lm')
