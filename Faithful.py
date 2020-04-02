3f5e0030abbdee5bae351376a5f292c36b74d999
Jbrak <joe@brakoniecki.com>
2020-04-02 17:16:38 +0100

    Played with Faithful Data

---
+++ Faithful.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+from plotnine import *
+from plotnine.data import *
+
+faithfuld
+
+ggplot(faithfuld,aes(x = 'eruptions', y = 'density', fill = 'waiting')) + geom_point(size = 4)
